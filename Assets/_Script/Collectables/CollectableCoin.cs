db7142d951f28db8b572a090839dd8e2279016ed
HyejeongOk <oghyejeong2@gmail.com>
2025-02-24 09:49:10 +0900

    v.0.0.21] feat : 코인 - 소멸 처리중

--- Assets/_Script/Collectables/CollectableCoin.cs
+++ Assets/_Script/Collectables/CollectableCoin.cs
@@ -1,8 +1,13 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CollectableCoin : Collectable
 {
+   // public Transform pivot;
+
+   
+
     // 해당 코인 증가량
     public uint Add = 1;
 
@@ -16,19 +21,15 @@ public class CollectableCoin : Collectable
         transform.SetPositionAndRotation(pos, Quaternion.identity);
     }
 
-    Sequence _seqInfo;
     public override void Collect()
     {
         GameManager.coins += Add;
 
-        // _seqInfo = DOTween.Sequence();
-        
-        // _seqInfo.Append(tmcoin.transform.DOScale(1.2f, duration * 0.1f));
-        // _seqInfo.Append(tmInfomation.transform.DOScale(1f, duration * 0.4f));
-        // _seqInfo.AppendInterval(duration*0.2f);
-        // _seqInfo.Append(tmInfomation.transform.DOScale(0f, duration * 0.1f));
+        transform.SetParent(null);
 
-        // this를 쓰명 CollectableCoin만, gameObject를 쓰면 전부 다 지워짐
+        transform.DOScale(1.2f, 0.25f)
+            .OnComplete(() => transform.DOScale(0f, 0.2f)
+            .OnComplete(() => Destroy(gameObject)));
         Destroy(gameObject);
     }
 }
